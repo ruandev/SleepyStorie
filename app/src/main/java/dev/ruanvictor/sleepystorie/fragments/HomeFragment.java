274f130d96685b70daaf23293c2fe4dcf354bcd2
Ruan Victor <ruan.techno@gmail.com>
2020-06-03 15:48:23 -0300

    Ajustes

--- app/src/main/java/dev/ruanvictor/sleepystorie/fragments/HomeFragment.java
+++ app/src/main/java/dev/ruanvictor/sleepystorie/fragments/HomeFragment.java
@@ -1,16 +1,24 @@
 package dev.ruanvictor.sleepystorie.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.List;
 
+import dev.ruanvictor.sleepystorie.MainActivity;
 import dev.ruanvictor.sleepystorie.R;
 import dev.ruanvictor.sleepystorie.containers.RecommendationContainer;
 import dev.ruanvictor.sleepystorie.model.Book;
@@ -21,6 +29,7 @@ import dev.ruanvictor.sleepystorie.model.Book;
 public class HomeFragment extends Fragment {
 
     List<Book> books;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -31,6 +40,7 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        setHasOptionsMenu(true);
 
         Button buttonInfo = view.findViewById(R.id.buttonInfo);
 
@@ -71,5 +81,18 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_fragment, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.new_item) {
+            mAuth.signOut();
+            startActivity(new Intent(getContext(), MainActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
